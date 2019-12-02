7660d7502ac666b24be32ca6fffe1ea3c5115f45
Khaled Daoudieh <khaled.dawodieh@gmail.com>
2019-12-02 10:42:16 +0100

    + date-packer, animation and calender

---
+++ node_modules/react-native-calendario/src/index.js
@@ -0,0 +1,9 @@
+/* @flow */
+import Calendar from './components/Calendar';
+
+export default Calendar;
+
+export type { CalendarType } from './types';
+export type { DayType } from './types';
+export type { ThemeType } from './types';
+export type { LocaleType } from './types';
